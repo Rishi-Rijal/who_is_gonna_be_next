8f3571e8fce8d5d490862ef2887d4e6178864a5f
Rishi <ramrijalrishi@gmail.com>
2026-03-31 15:53:39 +0300

    feat: add poll router to main application for handling poll-related requests

--- backend/app.ts
+++ backend/app.ts
@@ -2,13 +2,13 @@ import express from "express";
 import { clerkMiddleware } from "@clerk/express";
 import { attachUser } from "./src/features/auth/auth.middleware";
 import { arresteeRouter } from "./src/features/arrestee/arrestee.route";
+import { pollRouter } from "./src/features/poll/poll.route";
 import { apiResponse } from "./src/shared/utils/apiResponse";
 import { errorHandler } from "./src/shared/middleware/errorHandler";
-import { authRouter} from "./src/features/auth/auth.router";
+import { authRouter } from "./src/features/auth/auth.router";
 import { corsService } from "./src/shared/utils/cors";
 
 const app = express();
-
 
 app.use(corsService);
 app.use(express.json());
@@ -17,6 +17,7 @@ app.use(clerkMiddleware());
 app.use(attachUser);
 
 app.use("/api/v1/arrestee", arresteeRouter);
+app.use("/api/v1/poll", pollRouter);
 app.use("/api/v1/auth", authRouter);
 
 app.get("/health", (req, res) => {
